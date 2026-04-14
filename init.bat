a00f2aa22a0bb55c14505c90a648cb04294cee76
Adam Grzeszczuk <103129308+adamu86@users.noreply.github.com>
2026-04-14 20:45:28 +0200

    configs

--- init.bat
+++ init.bat
@@ -4,6 +4,6 @@ docker build -t recbole-env .
 
 @REM docker run -it --rm --name recbole-env --gpus all recbole-env
 
-docker run --memory=24g --memory-swap=48g --gpus all -it --rm -v "C:\Users\User\Documents\AdamkaRzeczy\GitHub\RecBole:/root/RecBole" recbole-env
+docker run --memory=24g --memory-swap=48g --gpus all -it --rm -v "C:\Users\User\Documents\AdamkaRzeczy\MasterDegree\RecBole:/root/RecBole" recbole-env
 
 pause
