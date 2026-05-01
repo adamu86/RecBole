2ba97cf62738350d9da6b6108be1033be06dbb5e
Adam Grzeszczuk <103129308+adamu86@users.noreply.github.com>
2026-05-01 02:33:01 +0200

    test results table improvement

--- app/frontend/src/App.vue
+++ app/frontend/src/App.vue
@@ -331,15 +331,31 @@ onMounted(init);
                   <h2 class="column-title">Eval. test results (1 GT)</h2>
                   <h2 class="column-title">Eval. test results (N GT)</h2>
                 </div>
-                <div class="column bg-white p-2">
-                  <div v-for="(val, metric) in (Object.values(currentMetricsTable.results_1)[0] ?? {})" :key="metric" >
-                    {{ metric }}: {{ val }}
-                  </div>
+                <div class="column bg-white p-2 grid grid-cols-[1fr_auto_auto]">
+                  <template v-for="(val, metric) in (Object.values(currentMetricsTable.results_1)[0] ?? {})" :key="metric">
+                    <div class="text-right px-2 py-1">
+                      {{ metric.split("@")[0] }}
+                    </div>
+                    <div class="px-2 py-1 border-x-[1px] border-x-gray-300">
+                      @{{ metric.split("@")[1] }}
+                    </div>
+                    <div class="px-2 py-1 font-medium text-left">
+                      {{ val }}
+                    </div>
+                  </template>
                 </div>
-                <div class="column bg-white p-2">
-                  <div v-for="(val, metric) in (Object.values(currentMetricsTable.results_N)[0] ?? {})" :key="metric">
-                    {{ metric }}: {{ val }}
-                  </div>
+                <div class="column bg-white p-2 grid grid-cols-[1fr_auto_auto]">
+                  <template v-for="(val, metric) in (Object.values(currentMetricsTable.results_N)[0] ?? {})" :key="metric">
+                    <div class="text-right px-2 py-1">
+                      {{ metric.split("@")[0] }}
+                    </div>
+                    <div class="px-2 py-1 border-x-[1px] border-x-gray-300">
+                      @{{ metric.split("@")[1] }}
+                    </div>
+                    <div class="px-2 py-1 font-medium text-left">
+                      {{ val }}
+                    </div>
+                  </template>
                 </div>
               </div>
             </div>
