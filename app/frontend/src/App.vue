13a4f85ad922026ac11a74375c3d0d56e4d6cdf3
Adam Grzeszczuk <103129308+adamu86@users.noreply.github.com>
2026-05-01 02:36:41 +0200

    -

--- app/frontend/src/App.vue
+++ app/frontend/src/App.vue
@@ -331,7 +331,7 @@ onMounted(init);
                   <h2 class="column-title">Eval. test results (1 GT)</h2>
                   <h2 class="column-title">Eval. test results (N GT)</h2>
                 </div>
-                <div class="column bg-white p-2 grid grid-cols-[1fr_auto_auto]">
+                <div class="column bg-white grid grid-cols-[1fr_auto_auto]">
                   <template v-for="(val, metric) in (Object.values(currentMetricsTable.results_1)[0] ?? {})" :key="metric">
                     <div class="text-right px-2 py-1">
                       {{ metric.split("@")[0] }}
@@ -340,11 +340,11 @@ onMounted(init);
                       @{{ metric.split("@")[1] }}
                     </div>
                     <div class="px-2 py-1 font-medium text-left">
-                      {{ val }}
+                      {{ val.toFixed(3) }}
                     </div>
                   </template>
                 </div>
-                <div class="column bg-white p-2 grid grid-cols-[1fr_auto_auto]">
+                <div class="column bg-white grid grid-cols-[1fr_auto_auto]">
                   <template v-for="(val, metric) in (Object.values(currentMetricsTable.results_N)[0] ?? {})" :key="metric">
                     <div class="text-right px-2 py-1">
                       {{ metric.split("@")[0] }}
@@ -353,7 +353,7 @@ onMounted(init);
                       @{{ metric.split("@")[1] }}
                     </div>
                     <div class="px-2 py-1 font-medium text-left">
-                      {{ val }}
+                      {{ val.toFixed(3) }}
                     </div>
                   </template>
                 </div>
