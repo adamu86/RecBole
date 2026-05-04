ca2ebbb5587cc4aad285966d6e9aaaa51d7434e8
Adam Grzeszczuk <103129308+adamu86@users.noreply.github.com>
2026-05-05 01:09:03 +0200

    -

--- app/backend/api.py
+++ app/backend/api.py
@@ -88,11 +88,9 @@ def load_model_names():
 
 def load_model(model_path=load_model_names()[0]):
     global config, model, dataset, train_data, valid_data, test_data, LOG_FILE
-    os.environ["CUDA_VISIBLE_DEVICES"] = ""
     files_before = set(Path("log").glob(f"**/*.log"))
     config, model, dataset, train_data, valid_data, test_data = load_data_and_model(
         model_file=model_path,
-        config_dict={"device": "cpu"}
     )
     log_dir = Path("log") / config['model']
     escaped_prefix = glob_module.escape(str(log_dir / f"{config['model']}-{config['dataset']}-"))
