4c6f3ff844e6f05d3b32338a8785c7e20b5a7599
Adam Grzeszczuk <103129308+adamu86@users.noreply.github.com>
2026-08-12 00:22:03 +0200

    -

--- run_recbole.py
+++ run_recbole.py
@@ -99,7 +99,7 @@ dataset_dir = Path("dataset")
 dataset_dict = {
     f"{p.name}": f"{p.name}"
     for p in dataset_dir.iterdir()
-    if p.is_dir()
+    if p.is_dir() and p.name.startswith("lastfm1k")
 }
 logger = getLogger()
 
